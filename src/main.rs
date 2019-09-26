d8ed3cbdfb43b4f6c2664d31c937aaa43ba57a59
Matt Redmond <me@mattredmond.com>
2019-09-26 11:59:27 +0930

    Part 2: Generalising

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,30 @@ const SCREEN_HEIGHT: i32 = 50;
 
 const LIMIT_FPS: i32 = 20;
 
-fn handle_keys(root: &mut Root, player_x: &mut i32, player_y: &mut i32) -> bool {
+struct Object {
+    x: i32,
+    y: i32,
+    char: char,
+    color: Color,
+}
+
+impl Object {
+    pub fn new(x: i32, y: i32, char: char, color: Color) -> Self {
+        Object { x, y, char, color }
+    }
+
+    pub fn move_by(&mut self, dx: i32, dy: i32) {
+        self.x += dx;
+        self.y += dy;
+    }
+
+    pub fn draw(&self, con: &mut dyn Console) {
+        con.set_default_foreground(self.color);
+        con.put_char(self.x, self.y, self.char, BackgroundFlag::None);
+    }
+}
+
+fn handle_keys(root: &mut Root, player: &mut Object) -> bool {
     use tcod::input::Key;
     use tcod::input::KeyCode::*;
 
@@ -18,10 +41,10 @@ fn handle_keys(root: &mut Root, player_x: &mut i32, player_y: &mut i32) -> bool 
             root.set_fullscreen(!fullscreen);
         }
         Key { code: Escape, .. } => return true,
-        Key { code: Up, .. } => *player_y -= 1,
-        Key { code: Down, .. } => *player_y += 1,
-        Key { code: Left, .. } => *player_x -= 1,
-        Key { code: Right, .. } => *player_x += 1,
+        Key { code: Up, .. } => player.move_by(0, -1),
+        Key { code: Down, .. } => player.move_by(0, 1),
+        Key { code: Left, .. } => player.move_by(-1, 0),
+        Key { code: Right, .. } => player.move_by(1, 0),
         _ => {}
     }
 
@@ -29,6 +52,8 @@ fn handle_keys(root: &mut Root, player_x: &mut i32, player_y: &mut i32) -> bool 
 }
 
 fn main() {
+    tcod::system::set_fps(LIMIT_FPS);
+
     let mut root = Root::initializer()
         .font("arial10x10.png", FontLayout::Tcod)
         .font_type(Greyscale)
@@ -36,18 +61,26 @@ fn main() {
         .title("Rusty Blade")
         .init();
 
-    tcod::system::set_fps(LIMIT_FPS);
-
-    let mut player_x: i32 = SCREEN_WIDTH / 2;
-    let mut player_y: i32 = SCREEN_HEIGHT / 2;
-
+    let mut con = Offscreen::new(SCREEN_WIDTH, SCREEN_HEIGHT);
+    let player = Object::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, '@', WHITE);
+    let npc = Object::new(SCREEN_WIDTH / 2 - 5, SCREEN_HEIGHT / 2, '@', YELLOW);
+    let mut objects = [player, npc];
 
     while !root.window_closed() {
-        root.set_default_foreground(WHITE);
-        root.clear();
-        root.put_char(player_x, player_y, '@', BackgroundFlag::None);
+        con.clear();
+        for object in &objects {
+            object.draw(&mut con);
+        }
+        blit(&mut con,
+             (0, 0),
+             (SCREEN_WIDTH, SCREEN_HEIGHT),
+             &mut root,
+             (0, 0),
+             1.0,
+             1.0);
         root.flush();
-        let exit = handle_keys(&mut root, &mut player_x, &mut player_y);
+        let player = &mut objects[0];
+        let exit = handle_keys(&mut root, player);
         if exit {
             break;
         }
