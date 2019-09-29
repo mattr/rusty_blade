7e97f47f513e204cc1deacecac021103550b5438
Matt Redmond <me@mattredmond.com>
2019-09-29 22:58:35 +0930

    Part 6: Going Berserk - The Components

--- src/main.rs
+++ src/main.rs
@@ -47,6 +47,17 @@ enum PlayerAction {
     Exit,
 }
 
+#[derive(Clone, Copy, Debug, PartialEq)]
+struct Fighter {
+    max_hp: i32,
+    hp: i32,
+    defense: i32,
+    power: i32,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+struct Ai;
+
 #[derive(Copy, Clone, Debug)]
 struct Rect {
     x1: i32,
@@ -104,11 +115,13 @@ struct Object {
     color: Color,
     blocks: bool,
     alive: bool,
+    fighter: Option<Fighter>,
+    ai: Option<Ai>,
 }
 
 impl Object {
     pub fn new(x: i32, y: i32, char: char, name: &str, color: Color, blocks: bool) -> Self {
-        Object { x, y, char, name: name.into(), color, blocks, alive: false }
+        Object { x, y, char, name: name.into(), color, blocks, alive: false, fighter: None, ai: None }
     }
 
     pub fn draw(&self, con: &mut dyn Console) {
@@ -155,9 +168,25 @@ fn place_objects(room: Rect, objects: &mut Vec<Object>) {
         let x = rand::thread_rng().gen_range(room.x1 + 1, room.x2);
         let y = rand::thread_rng().gen_range(room.y1 + 1, room.y2);
         let mut monster = if rand::random::<f32>() < 0.8 {
-            Object::new(x, y, 'o', "Orc", DARK_GREEN, true)
+            let mut orc = Object::new(x, y, 'o', "Orc", DARK_GREEN, true);
+            orc.fighter = Some(Fighter {
+                max_hp: 10,
+                hp: 10,
+                defense: 0,
+                power: 3,
+            });
+            orc.ai = Some(Ai);
+            orc
         } else {
-            Object::new(x, y, 'T', "Troll", DARKER_GREEN, true)
+            let mut troll = Object::new(x, y, 'T', "Troll", DARKER_GREEN, true);
+            troll.fighter = Some(Fighter {
+                max_hp: 16,
+                hp: 16,
+                defense: 1,
+                power: 4,
+            });
+            troll.ai = Some(Ai);
+            troll
         };
         monster.alive = true;
         objects.push(monster);
@@ -328,6 +357,12 @@ fn main() {
     let mut con = Offscreen::new(MAP_WIDTH, MAP_HEIGHT);
     let mut player = Object::new(0, 0, '@', "player", WHITE, true);
     player.alive = true;
+    player.fighter = Some(Fighter {
+        max_hp: 30,
+        hp: 30,
+        defense: 2,
+        power: 5,
+    });
     let mut objects = vec![player];
     let mut map = make_map(&mut objects);
 
